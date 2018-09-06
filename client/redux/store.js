3515ef8cf0edaf1a46a759bdbeceea8fb44d36fa
johnlatona <johnbl24@aol.com>
2018-09-06 16:43:20 -0400

    first commit

---
+++ client/redux/store.js
@@ -0,0 +1,23 @@
+import { createStore, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import { createLogger } from 'redux-logger';
+
+const initialState = {
+
+}
+
+const reducer = (state = initialState, action) => {
+  switch(action.type) {
+    default:
+      return state;
+  }
+}
+
+const store = createStore(
+  reducer,
+  applyMiddleware(
+    thunkMiddleware, createLogger()
+  )
+);
+
+export default store;
